e1b07d1c0e7b3c24eb0347de687c7342f80648ad
NichoAndianto <03nicholasa@gmail.com>
2025-10-08 19:09:37 +0700

    Added Changes and Fixing
    -Change Font
    -And the Other

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,64 +1,95 @@
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 using TMPro;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+using System.Collections;          
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-    public int playerScoreL, playerScoreR;
-    public TMPro.TextMeshPro textMeshPro;
-    public TMP_Text textScoreL, textScoreR;
+
+    [Header("Player Scores")]
+    public int playerScoreL;
+    public int playerScoreR;
+
+    [Header("UI References")]
+    public TMP_Text textScoreL;
+    public TMP_Text textScoreR;
     public TMP_Text winText;
-    public GameObject winCon;
+    public GameObject winCon; 
 
-    private void Start()
+    private void Awake()
     {
-        textScoreL.text = playerScoreL.ToString();
-    }
-    public void Awake()
-    {
-    if(Instance == null)
+        
+        if (Instance == null)
         {
-        Instance = this;
-        }
-    else
-        {
-        Destroy(gameObject);
-        }
-
-    } 
-    public void AddScore(string wallName)
-    {
-        if(wallName == "LeftLine")
-        {
-            playerScoreR = playerScoreR + 10;
-            textScoreR.text = playerScoreR.ToString();
-            ScoreCheck();
+            Instance = this;
         }
         else
         {
-            playerScoreL = playerScoreL + 10;
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        
+        textScoreL.text = playerScoreL.ToString();
+        textScoreR.text = playerScoreR.ToString();
+
+        if (winCon != null)
+            winCon.SetActive(false);
+    }
+
+    public void AddScore(string wallName)
+    {
+        
+        if (wallName == "LeftLine")
+        {
+            playerScoreR += 10;
+            textScoreR.text = playerScoreR.ToString();
+            ScoreCheck();
+        }
+        else if (wallName == "RightLine")
+        {
+            playerScoreL += 10;
             textScoreL.text = playerScoreL.ToString();
             ScoreCheck();
         }
     }
-    public void ScoreCheck()
+
+    private void ScoreCheck()
     {
-        if(playerScoreR == 20) 
-        WinCondition("Player R Win!");
-        
-        else if(playerScoreL == 20) 
-        WinCondition("Player L Win!");
-    }
     
-        private void WinCondition(string text)
+        if (playerScoreR >= 50)
         {
-            winCon.SetActive(true);
-            winText.text = text;
+            WinCondition("Player R Wins!");
+        }
+    
+        else if (playerScoreL >= 50)
+        {
+            WinCondition("Player L Wins!");
         }
     }
-    
 
+    private void WinCondition(string text)
+    {
+        
+        if (winCon != null)
+            winCon.SetActive(true);
 
+        if (winText != null)
+            winText.text = text;
+
+        
+        StartCoroutine(ReturnToMainMenuCoroutine());
+    }
+
+    private IEnumerator ReturnToMainMenuCoroutine()
+    {
+        yield return new WaitForSecondsRealtime(3f);
+
+        
+        SceneManager.LoadScene("MainMenu");
+    }
+}
